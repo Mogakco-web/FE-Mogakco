be02c3fc4a2839e55c4b86a838e604eea5123223
bm4656 <newjin46@gmail.com>
2023-01-16 02:12:35 +0900

    feat: Header 컴포넌트

---
+++ src/components/Header.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import tw from 'tailwind-styled-components';
+const MemberNav = () => {
+  return (
+    <MenuContainer>
+      <Menu>
+        <Link to='/rank'>랭킹</Link>
+      </Menu>
+      <Menu>
+        <Link to='/timer'>타이머</Link>
+      </Menu>
+      <Menu>
+        <Link to='/todo'>투두리스트</Link>
+      </Menu>
+      <Menu>
+        <Link to='/cdtest'>코딩테스트</Link>
+      </Menu>
+      <Menu>
+        <Link to='/mypage'>마이페이지</Link>
+      </Menu>
+    </MenuContainer>
+  );
+};
+const GuestNav = () => {
+  return (
+    <MenuContainer>
+      <Menu>
+        <Link to='/rank'>랭킹</Link>
+      </Menu>
+      <Menu>
+        <Link to='/notice'>공지사항</Link>
+      </Menu>
+      <Menu>
+        <Link to='/q&a'>Q&A</Link>
+      </Menu>
+    </MenuContainer>
+  );
+};
+const Header = () => {
+  return (
+    <MyHeader>
+      <Logo
+        src={`${process.env.PUBLIC_URL}/mogakco_logo.png`}
+        alt='모각코 로고'
+      />
+      <nav>
+        <GuestNav />
+        {/* <MemberNav /> */}
+      </nav>
+      {/* <nav>{isLogged ? <MemberNav /> : <GuestNav />}</nav> */}
+    </MyHeader>
+  );
+};
+
+export default Header;
+
+const MyHeader = tw.div`
+bg-cpColor
+w-screen
+h-16
+p-3
+flex
+justify-between
+items-center
+`;
+const Logo = tw.img`
+w-12
+`;
+const MenuContainer = tw.div`
+flex
+gap-3
+`;
+const Menu = tw.li`
+list-none
+cursor-pointer
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import useStore from '../store/userStore';
+import Header from '../components/Header';
 import tw from 'tailwind-styled-components';
 
 const Home = () => {
   const { isDark, handleIsDark } = useStore();
   return (
     <Wrapper>
-      <Hi>테일윈드 테스트</Hi>
+      <Header />
       <p>Zustand TEST : 지금 화면은 {isDark ? '다크' : '화이트'} 모드이다</p>
       <button onClick={handleIsDark}>클릭시 화면 모드 변경</button>
     </Wrapper>
@@ -14,11 +15,6 @@ const Home = () => {
 };
 
 const Wrapper = tw.div`
-`;
-
-const Hi = tw.div`
-w-fit
-text-cpColor
 `;
 
 export default Home;
