d9f565e6e344bed46d2344e2fee75036fac1d159
bm4656 <newjin46@gmail.com>
2023-02-05 01:44:47 +0900

    feat: 로그인 상태관리 추가(header)

--- src/pages/CallBack.tsx
+++ src/pages/CallBack.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import userStore from '../store/userStore';
 
 const CallBack = () => {
-  const { isLogin, handleIsLogin, setUserInfo } = userStore();
+  const { handleIsLogin, setUserInfo } = userStore();
   const navigate = useNavigate();
   const code = getCode('code');
   const postCode = async () => {
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
+import userStore from '../store/userStore';
 
 const loginUrl = `https://github.com/login/oauth/authorize?client_id=aed31f0dad5ed858d765&scope=repo:status read:repo_hook user:email&redirect_uri=http://localhost:3000/callback`;
+
 const Login = () => {
+  const { handleIsLogin, setUserInfo } = userStore();
   return (
     <>
       <Button>
         <a href={loginUrl}>GitHub 아이디로 로그인</a>
+      </Button>
+      <Button
+        onClick={() => {
+          handleIsLogin();
+          setUserInfo('', '');
+        }}>
+        임시 로그아웃
       </Button>
     </>
   );
